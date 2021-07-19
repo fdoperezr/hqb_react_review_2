026e05ae03c782746dd2fd1bd00acf054c7b3eca
Fernando Pérez <fernando.perez@hqb.cl>
2021-07-19 09:44:35 -0400

    ejercicio completo

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export type Usuario = {
     nombreUsuario: string,
@@ -9,37 +9,70 @@ export type Usuario = {
 
 export default function Formulario() {
     const [usuario, setUsuario] = useState<Usuario>({
-        nombreUsuario: 'Nombre de prueba'
-    })
-    const [mensaje, setMensaje] = useState('')
+        nombreUsuario: 'Nombre de prueba',
+    });
+    const [mensaje, setMensaje] = useState('');
+
+    useEffect(() => {
+        if (usuario.estado) {
+            setMensaje('El usuario está activo');
+        }
+        else {
+            setMensaje('El usuario no está activo');
+        }
+    }, [usuario.estado]);
+
+    const hanlderInput = (event: any): void => {
+        setUsuario({
+            ...usuario,
+            [event.target.name]: event.target.value
+        })
+
+    };
+
+    const handleOnClick = () => {
+        alert(mensaje);
+    };
+
+    const handlerCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsuario({
+            ...usuario,
+            estado: event.target.value === 'true'
+        });
+    };
+
     return (
         <div className="container">
             <div className="d-flex justify-content-center">
                 <div className="col-6">
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlInput1" className="form-label">Usuario</label>
-                        <input type="text" className="form-control" id="exampleFormControlInput1" disabled={true} value={usuario.nombreUsuario}/>
+                        <input type="text" className="form-control" id="exampleFormControlInput1" disabled={true} value={usuario.nombreUsuario} onChange={hanlderInput}
+                            name="nombreUsuario" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlInput2" className="form-label">Nombre</label>
-                        <input type="text" className="form-control" id="exampleFormControlInput2" />
+                        <input type="text" className="form-control" id="exampleFormControlInput2" value={usuario.nombre} onChange={hanlderInput}
+                            name="nombre" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlInput3" className="form-label">Apellido</label>
-                        <input type="text" className="form-control" id="exampleFormControlInput3" />
+                        <input type="text" className="form-control" id="exampleFormControlInput3" value={usuario.apellido} onChange={hanlderInput}
+                            name="apellido" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlInput4" className="form-label">Estado</label>
-                        <br/>
+                        <br />
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value={'true'} checked={usuario.estado} onChange={handlerCheck} />
                             <label className="form-check-label" htmlFor="inlineRadio1">SI</label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value={'false'} checked={!usuario.estado} onChange={handlerCheck}/>
                             <label className="form-check-label" htmlFor="inlineRadio2">NO</label>
                         </div>
                     </div>
+                    <button type="button" className="btn btn-primary btn-lg btn-block" onClick={handleOnClick}>Enviar</button>
                 </div>
             </div>
         </div>
